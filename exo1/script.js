9bf0678c0d265698085bb92cdef47cdf586c19c0
balaman-hala <balamanhala76@gmail.com>
2025-11-04 18:38:19 +0100

    push de mon travail

---
+++ exo1/script.js
@@ -0,0 +1,25 @@
+"use strict";
+function F1() {
+    // Ici on utilise une fonction d'affichage dans la console (vu dans le slide 33)
+    console.log("Jhon")
+}
+function F2() {
+    // Ici on utilise une fonction d'affichage dans la console (vu dans le slide 33)
+    console.log("Doe")
+}
+function F3() {
+    // Ici on utilise une fonction d'affichage dans la console (vu dans le slide 33)
+    console.log("Jhon-Doe")
+}
+function F4() {
+    // Ici on utilise une fonction d'affichage dans la console (vu dans le slide 33)
+    console.log(30)
+}
+function F5() {
+    // Ici on utilise une fonction d'affichage dans la console (vu dans le slide 33)
+    console.log(15)
+}
+function F6() {
+    // Ici on utilise une fonction d'affichage dans la console (vu dans le slide 33)
+    console.log(3.14)
+}
